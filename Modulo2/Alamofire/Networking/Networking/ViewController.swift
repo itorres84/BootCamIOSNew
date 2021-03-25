b0003940d053e1a9339b0cea92060257c10ef352
Israel Torres <israeltorres27@gmail.com>
2021-03-24 21:45:15 -0600

    add cocoapod whit bundler

---
+++ Modulo2/Alamofire/Networking/Networking/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Israel Torres Alvarado on 24/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '11.0'
+use_frameworks!
+workspace 'BootCamIOS'
+
+
+target 'Networking' do
+  
+  project 'Modulo2/Alamofire/Networking/Networking'
+  
+  pod 'Alamofire', '~> 5.2'
+
+
+end
