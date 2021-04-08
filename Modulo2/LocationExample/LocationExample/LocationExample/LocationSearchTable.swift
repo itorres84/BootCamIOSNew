d6ddaa66bff3755a9d5ea6f99044eb68e2526d79
Israel Torres <israeltorres27@gmail.com>
2021-04-07 21:23:03 -0500

    mapKit location complement

---
+++ Modulo2/LocationExample/LocationExample/LocationExample/LocationSearchTable.swift
@@ -0,0 +1,161 @@
+//
+//  LocationSearchTable.swift
+//  LocationExample
+//
+//  Created by Israel Torres Alvarado on 07/04/21.
+//
+
+import UIKit
+import MapKit
+
+
+// MARK: Protocolos
+protocol Cart: Any {
+    
+    var ruedas: Int { get set }
+    var puertas: Int { get }
+    
+    var color: String { get set }
+    func acelerar()
+}
+
+extension Cart {
+    var color: String  {
+        get { return "default color" } set {}
+    }
+}
+
+struct FordCart: Cart {
+
+    var ruedas: Int
+    var puertas: Int
+    
+    var seguro: Bool = false
+    
+    init(ruedas: Int, puertas: Int) {
+        self.ruedas = ruedas
+        self.puertas = puertas
+    }
+    
+    func acelerar() {
+        print("ford.. acelera")
+    }
+}
+
+class AudiCart: Cart {
+    
+    var ruedas: Int
+    var puertas: Int
+    
+    init(ruedas: Int, puertas: Int) {
+        self.ruedas = ruedas
+        self.puertas = puertas
+    }
+    
+    func acelerar() {
+        print("Audi.. acelera")
+    }
+
+}
+
+class MazdaCart: Cart {
+        
+    var ruedas: Int
+    var puertas: Int
+    
+    var color: String = "Negro"
+    
+    init(ruedas: Int, puertas: Int) {
+        self.ruedas = ruedas
+        self.puertas = puertas
+    }
+    
+    func acelerar() {
+        print("Mazda.. acelera")
+        print("color: \(self.color)")
+    }
+}
+
+//var cart: Cart = MazdaCart(ruedas: 4, puertas: 2)
+//cart.puertas = 5
+//cart.ruedas = 5
+
+protocol LocationSearchTableShowResult {
+    func showResult(placemark: MKPlacemark)
+}
+
+class LocationSearchTable: UITableViewController, UISearchResultsUpdating {
+    
+    var mapView: MKMapView? = nil
+    
+    var delegate: LocationSearchTableShowResult?
+    
+    var matchItems: [MKMapItem] = [] {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchItems.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = UITableViewCell()
+        let item = matchItems[indexPath.row].placemark
+        let adress = "\(item.thoroughfare ?? ""), \(item.locality ?? ""), \(item.subLocality ?? ""), \(item.administrativeArea ?? "")"
+        cell.textLabel?.text = "\(item.name ?? "") \n \(adress)"
+        cell.textLabel?.numberOfLines = 0
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let placemark: MKPlacemark = matchItems[indexPath.row].placemark
+        
+        dismiss(animated: true) {
+            self.delegate?.showResult(placemark: placemark)
+        }
+    }
+
+    // MARK: - UISearchResultsUpdating
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard  let mapView = self.mapView, let searchText = searchController.searchBar.text else {
+            return
+        }
+        
+        //Step 1
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchText
+        request.region = mapView.region
+        
+        //Step 2
+        let search = MKLocalSearch(request: request)
+        
+        //Step 3
+        search.start { (res, _ ) in
+            
+            guard let response = res else {
+                return
+            }
+            
+            self.matchItems = response.mapItems
+        }
+        
+        
+        
+    }
+
+}
--- Modulo2/LocationExample/LocationExample/LocationExample/ViewController.swift
+++ Modulo2/LocationExample/LocationExample/LocationExample/ViewController.swift
@@ -8,87 +8,115 @@
 import UIKit
 import CoreLocation
 import MapKit
+import Speech
 
-class ViewController: UIViewController {
-
-//    private let mapView = MKMapView(frame: .zero)
-    private let rangeInMeters: Double = 25000
+class ViewController: UIViewController, LocationSearchTableShowResult {
+    
+    private let rangeInMeters: Double = 5000
         
+    var resultSearchController: UISearchController? = nil
+    
+    var selectedPin:MKPlacemark? = nil
+    var route: MKRoute?
+    var stepCounter: Int = 0
+    
+    let synthesizer = AVSpeechSynthesizer()
+    
     @IBOutlet weak var mapView: MKMapView! {
         didSet {
-            //mapView.mapType = .satelliteFlyover
             mapView.showsUserLocation = true
+            mapView.delegate = self
         }
     }
     
-    @IBOutlet weak var txtAddress: UITextField!
-    @IBOutlet weak var lblAddress: UILabel!
+//    @IBOutlet weak var txtAddress: UITextField!
+//    @IBOutlet weak var lblAddress: UILabel!
     
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        //appDelegate.locationManager.delegate = self
-        //appDelegate.locationManager.distanceFilter  = 30
-        //appDelegate.locationManager.allowsBackgroundLocationUpdates = true
-        //appDelegate.locationManager.startUpdatingLocation()
-        //layoutUI()
         checkLocationServices()
-        
         setCustomAnnotation()
+        
+        guard let storyboard = self.storyboard,
+              let locationSearchTable = storyboard.instantiateViewController(identifier: "LocationSearchTable") as? LocationSearchTable
+              else {
+            return
+        }
+        
+        locationSearchTable.mapView = mapView
+        locationSearchTable.delegate = self
+        
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        guard let searchBar = resultSearchController?.searchBar else {
+            return
+        }
+        
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        
+        self.navigationItem.searchController = resultSearchController
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = true
+        resultSearchController?.obscuresBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        
+        
+        
     }
     
-    @IBAction func searchAddress(_ sender: UIButton) {
-       
-        guard let searchAddress = txtAddress.text,
-              !searchAddress.isEmpty else { return }
-        
-        dump("search....\(searchAddress)")
-        
-        self.getCoordinate(forAddress: searchAddress)
-        
-    }
+//    @IBAction func searchAddress(_ sender: UIButton) {
+//
+//        guard let searchAddress = txtAddress.text,
+//              !searchAddress.isEmpty else { return }
+//        self.getCoordinate(forAddress: searchAddress)
+//
+//    }
     
     func getCoordinate(forAddress address: String) {
         
-        let request = MKLocalSearch.Request()
-        request.naturalLanguageQuery = address
-        request.region = mapView.region
-        let search = MKLocalSearch(request: request)
-        
-        search.start { (res, error) in
-            
-            guard let response = res else {
-                return
-            }
-            
-            dump(response.mapItems)
-            self.mapView.removeAnnotations(self.mapView.annotations)
-            
-            _ = response.mapItems.map({
-                let annotation = MKPointAnnotation()
-                annotation.coordinate = $0.placemark.coordinate
-                        annotation.title = $0.name
-                if let city = $0.placemark.locality,
-                   let state = $0.placemark.administrativeArea {
-                            annotation.subtitle = "\(city) \(state)"
-                }
-                
-                self.mapView.addAnnotation(annotation)
-                
-            })
-            
-            
-            
-        }
+//        //Step 1
+//        let request = MKLocalSearch.Request()
+//        request.naturalLanguageQuery = address
+//        request.region = mapView.region
+//
+//        //Step 2
+//        let search = MKLocalSearch(request: request)
+//
+//        //Step 3
+//        search.start { (res, _ ) in
+//
+//            guard let response = res else {
+//                return
+//            }
+//
+//            self.mapView.removeAnnotations(self.mapView.annotations)
+//
+//            _ = response.mapItems.map({
+//
+//                let annotation = MKPointAnnotation()
+//                annotation.coordinate = $0.placemark.coordinate
+//                        annotation.title = $0.name
+//                if let city = $0.placemark.locality,
+//                   let state = $0.placemark.administrativeArea {
+//                            annotation.subtitle = "\(city) \(state)"
+//                }
+//
+//                self.mapView.addAnnotation(annotation)
+//
+//            })
+//
+//        }
         
         
 //        let geocoder = CLGeocoder()
 //
 //        geocoder.geocodeAddressString(address) { (resultPlaces, error) in
+//
 //            if let err = error {
 //                print("Geocoding error: \(err.localizedDescription)")
 //            } else {
@@ -121,20 +149,7 @@ class ViewController: UIViewController {
 //        mapView.addAnnotation(airport)
         
     }
-    
-    
-      private func layoutUI() {
-//          mapView.translatesAutoresizingMaskIntoConstraints = false
-//          view.addSubview(mapView)
-//
-//          NSLayoutConstraint.activate([
-//              mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-//              mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-//              mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-//              mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-//          ])
-      }
-    
+        
     private func checkAuthorizationForLocation() {
         
         switch CLLocationManager.authorizationStatus() {
@@ -188,6 +203,90 @@ class ViewController: UIViewController {
         
         mapView.setRegion(coordinateRegion, animated: true)
     }
+    
+    func showResult(placemark: MKPlacemark) {
+        
+        selectedPin = placemark
+        
+        mapView.removeAnnotations(mapView.annotations)
+        
+        let annnotation = MKPointAnnotation()
+        
+        annnotation.coordinate = placemark.coordinate
+        annnotation.title = placemark.name
+        
+        if let city = placemark.locality,
+           let state = placemark.administrativeArea {
+            annnotation.subtitle = "\(city) \(state)"
+        }
+        
+        mapView.addAnnotation(annnotation)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: placemark.coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+    
+    }
+    
+    fileprivate func mapToRoute(destionationCoordinate: CLLocationCoordinate2D) {
+        
+        guard let sourceCoordinate = locationManager.location?.coordinate else {
+            return
+        }
+        
+        let sourcePlaceMarck = MKPlacemark(coordinate: sourceCoordinate)
+        let destiationPlaceMarck = MKPlacemark(coordinate: destionationCoordinate)
+        
+        let sourceItem = MKMapItem(placemark: sourcePlaceMarck)
+        let destinationItem = MKMapItem(placemark: destiationPlaceMarck)
+        
+        
+        let routeRequest = MKDirections.Request()
+        routeRequest.source = sourceItem
+        routeRequest.destination = destinationItem
+        routeRequest.transportType = .automobile
+        
+        let directions = MKDirections(request: routeRequest)
+        directions.calculate { (response, err) in
+            
+            if let error = err {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let response = response, let route = response.routes.first else { return }
+            
+            self.route = route
+            
+            self.mapView.addOverlay(route.polyline)
+            self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets(top: 16, left:16, bottom: 16, right: 16), animated: true)
+            self.getRouteSteps(routes: route)
+            
+        }
+
+    }
+    
+    fileprivate func getRouteSteps(routes: MKRoute) {
+        
+        for monitoredRegion in locationManager.monitoredRegions {
+            locationManager.stopMonitoring(for: monitoredRegion)
+        }
+        
+        let steps = routes.steps.enumerated()
+        let stepsDos = routes.steps
+        
+        _ = steps.map({ (index, elemet)  in
+            let region = CLCircularRegion(center: elemet.polyline.coordinate, radius: 20, identifier: "\(index)")
+            locationManager.startMonitoring(for: region)
+        })
+        
+        stepCounter += 1
+        let initialMessange = "En \(stepsDos[stepCounter].distance) metros  \(stepsDos[stepCounter].instructions), luego en \(stepsDos[stepCounter + 1].distance) metros, \(stepsDos[stepCounter + 2].instructions)"
+        
+        let spetch = AVSpeechUtterance(string: initialMessange)
+        spetch.voice = AVSpeechSynthesisVoice(language: "es-MX")
+        synthesizer.speak(spetch)
+        //startNavigation()
+    }
 
 }
 
@@ -207,4 +306,59 @@ extension ViewController: CLLocationManagerDelegate {
     }
     
 }
+
+extension ViewController: MKMapViewDelegate {
+    // 1
+    func mapView(
+      _ mapView: MKMapView,
+      viewFor annotation: MKAnnotation
+    ) -> MKAnnotationView? {
+      // 2
+      guard let annotation = annotation as? MKPointAnnotation else {
+        return nil
+      }
+      // 3
+      let identifier = "pin"
+      var view: MKMarkerAnnotationView
+      // 4
+      if let dequeuedView = mapView.dequeueReusableAnnotationView(
+        withIdentifier: identifier) as? MKMarkerAnnotationView {
+        dequeuedView.annotation = annotation
+        view = dequeuedView
+      } else {
+        // 5
+        view = MKMarkerAnnotationView(
+          annotation: annotation,
+          reuseIdentifier: identifier)
+        view.canShowCallout = true
+        view.calloutOffset = CGPoint(x: -5, y: 5)
+        view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+      }
+      return view
+    }
+    
+    func mapView(
+      _ mapView: MKMapView,
+      annotationView view: MKAnnotationView,
+      calloutAccessoryControlTapped control: UIControl
+    ) {
+        
+      guard let anotation = view.annotation as? MKPointAnnotation else {
+        return
+      }
+        
+      mapToRoute(destionationCoordinate: anotation.coordinate)
+      
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = .systemBlue
+        return renderer
+        
+    }
+    
+}
+
 
